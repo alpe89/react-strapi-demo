db080e5130998911f940970f4139958c91d16a80
alpe89 <a.pertusi89@gmail.com>
2022-03-21 23:16:41 +0100

    added tailwind and augmented process interface

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="react-scripts" />
+// Extending ProcessEnv to have autocompletion of .env variables
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: 'development' | 'production' | 'test';
+    PUBLIC_URL: string;
+    API_KEY: string;
+    BASE_URL: string;
+  }
+}
