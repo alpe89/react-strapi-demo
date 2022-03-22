8e412c9176caa668370f076bbc03435c6048c109
alpe89 <a.pertusi89@gmail.com>
2022-03-23 00:26:31 +0100

    added offer image and fixed navbar

--- src/landing/Landing.tsx
+++ src/landing/Landing.tsx
@@ -2,16 +2,30 @@ import { useLayoutEffect, useState, VFC } from 'react';
 import { NavigationCarousel, NavigationPage, Slider } from './components';
 
 export const Landing: VFC = () => {
-  const [screenY, setScreenY] = useState(() => window.screenY);
+  const [scrollY, setScrollY] = useState(() => window.screenY);
 
   useLayoutEffect(() => {
-    setScreenY(window.screenY);
+    const updateY = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', updateY);
+
+    return () => {
+      window.removeEventListener('scroll', updateY);
+    };
   }, []);
 
   return (
     <main className="h-full w-full">
-      {screenY <= 564 ? <NavigationCarousel /> : <NavigationPage />}
+      {scrollY <= 564 ? <NavigationCarousel /> : <NavigationPage />}
       <Slider />
+      <div className="w-full h-[200px] my-32 flex justify-center content-center">
+        <img
+          src="https://www.edisonenergia.it/wps/wcm/connect/e44fe310-e404-4042-920e-90cccb498902/smartphone/MOBILE_Negozi_Edison+355x200.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE-e44fe310-e404-4042-920e-90cccb498902-smartphone-n-jtcYq"
+          alt="edison offer"
+        />
+      </div>
     </main>
   );
 };
