edb3eea84e6deed1f57c696f329ec23aab0a0789
alpe89 <a.pertusi89@gmail.com>
2022-03-24 10:03:54 +0100

    added navigation to offers pages

--- src/config/routes/routes.tsx
+++ src/config/routes/routes.tsx
@@ -1,11 +1,12 @@
 import { VFC } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Landing } from '../../landing';
-import { Offers } from '../../offers';
 
 export const AppRoutes: VFC = () => (
   <Routes>
     <Route path="/" element={<Landing />} />
-    <Route path="/offerte-luce" element={<Offers />} />
+    <Route path="/offerte/luce" element={<div>Offerta Luce</div>} />
+    <Route path="/offerte/luce-e-gas" element={<div>Offerta Luce e Gas</div>} />
+    <Route path="/offerte/gas" element={<div>Offerta Gas</div>} />
   </Routes>
 );
