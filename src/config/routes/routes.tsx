0f5a7c5f8b027a45b4f28a2703053b6a96a6e15f
Teech300 <taher.chaouachi@sysdata.it>
2022-03-25 11:34:17 +0100

    static component GasDetails

--- src/config/routes/routes.tsx
+++ src/config/routes/routes.tsx
@@ -1,13 +1,23 @@
 import { VFC } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Landing } from '../../landing';
+import { GasDetails } from '../../offers/GasDetails';
 import { LightDetails } from '../../offers/LightDetails';
 
 export const AppRoutes: VFC = () => (
   <Routes>
     <Route path="/" element={<Landing />} />
     <Route path="/offerte/luce" element={<LightDetails />} />
-    <Route path="/offerte/luce-e-gas" element={<div>Offerta Luce e Gas</div>} />
-    <Route path="/offerte/gas" element={<div>Offerta Gas</div>} />
+    <Route
+      path="/offerte/luce-e-gas"
+      element={
+        // eslint-disable-next-line react/jsx-wrap-multilines
+        <div>
+          <LightDetails />
+          <GasDetails />
+        </div>
+      }
+    />
+    <Route path="/offerte/gas" element={<GasDetails />} />
   </Routes>
 );
--- src/offers/GasDetails/GasDetails.tsx
+++ src/offers/GasDetails/GasDetails.tsx
@@ -4,34 +4,26 @@ import {
   OfferDetailItem,
 } from '../components';
 
-export const LightDetails = () => {
+export const GasDetails = () => {
   const url = window.location.href;
-  const textPrice = '€/kWh';
-  const textColor = 'textGreen';
+  const textPrice = '€/Smc';
+  const textColor = 'textBlue';
 
   return (
     <section className="flex flex-col justify-center gap-4 m-34 w-1/2 m-auto">
       <OfferDetailContainer color={textColor} urlProject={url}>
         <OfferDetailRow>
           <OfferDetailItem
-            price="0,301"
-            description="Unico prezzo per tutte le ore della giornata"
             color={textColor}
+            price="0,301"
             textPrice={textPrice}
           />
           <OfferDetailItem
-            price="0,301"
-            description="Unico prezzo per tutte le ore della giornata"
+            color={textColor}
+            textPrice={textPrice}
             index={1}
-            color={textColor}
-            textPrice={textPrice}
-          />
-        </OfferDetailRow>
-        <OfferDetailRow>
-          <OfferDetailItem
-            color={textColor}
-            title="Prezzo Monorario"
-            description="Unico prezzo per tutte le ore della giornata"
+            description="Con Edison Gas Prezzo Fisso12 blocchi il prezzo della materia prima per 12 mesi."
+            sideDescription
           />
         </OfferDetailRow>
       </OfferDetailContainer>
